6a48f53f31d33da6f952756736092eb63307e329
bleachgh <2911010267@qq.com>
2024-03-27 09:12:59 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ let userID = '8c550a42-88e0-45c4-96f9-2706d3041f0f';
 
 let proxyIP = '193.122.125.174';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'
 
-//let sub = '';// 留空则显示原版内容
+//let sub = '';// 留空则显示原版内容.
 let sub = '';// 内置优选订阅生成器，可自行搭建 https://github.com/cmliu/WorkerVless2sub
 let subconverter = 'api.v1.mk';// clash订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
